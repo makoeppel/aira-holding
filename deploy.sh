0813a5b006cd9ddacde2658f63c64a05eda040d4
makoeppel <koeppel.ma@googlemail.com>
2023-05-08 14:05:40 +0200

    update colors, add impressum

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env sh
 
-# abort on errors
-set -e
-
 # build
 npm run build
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git add dist -f
+cd dist
+git init
+git add . -f
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git subtree push --prefix dist origin gh-pages
+git push -f git@github.com:makoeppel/aira-holding.git main:gh-pages
+
+cd -
