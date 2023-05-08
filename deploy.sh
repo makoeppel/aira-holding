4e0727a8bc6d03eb4daafc7e60295376d4cd03f3
makoeppel <koeppel.ma@googlemail.com>
2023-05-08 12:07:45 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -6,13 +6,10 @@ set -e
 # build
 npm run build
 
-# navigate into the build output directory
-cd dist
-
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git init
+git add dist -f
 git add -A
 git commit -m 'deploy'
 
@@ -20,6 +17,4 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:makoeppel/aira-holding.git main:gh-pages
-
-cd -
+git subtree push --prefix dist origin gh-pages
