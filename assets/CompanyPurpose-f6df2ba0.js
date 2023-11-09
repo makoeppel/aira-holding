be3e2a2e933bd22aef190b99ce4d206c938db4fa
Janik <janik.koeppel@sap.com>
2023-11-09 12:33:18 +0100

    deploy pages

---
+++ assets/CompanyPurpose-f6df2ba0.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./HomeView-3eb5e27b.js";import{C as t}from"./HomeView-3eb5e27b.js";import"./index-c715a81a.js";import"./VAvatar-245e1ae6.js";export{t as CompanyPurposes,r as default};
---
+++ assets/TeamMember-bae3651b.js
@@ -0,0 +1,1 @@
+import{a as r}from"./HomeView-3eb5e27b.js";import{T as p}from"./HomeView-3eb5e27b.js";import"./index-c715a81a.js";import"./VAvatar-245e1ae6.js";export{p as TeamMembers,r as default};
